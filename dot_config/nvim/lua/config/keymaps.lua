248b2086d452333816c05bff7e1d6100936c0238
Özenç Bilgili <bilgiliozenc@gmail.com>
2023-11-12 15:56:58 +0300

    add more dotfiles

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+-- This file is automatically loaded by lazyvim.plugins.config
+
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("i", "jj", "<ESC>", {silent = true})
