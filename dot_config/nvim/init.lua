b79698d86fb1efc9aa64f13a1262bebbf0a7e708
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2024-03-10 16:55:39 +0300

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -135,10 +135,10 @@ require('lazy').setup({
 
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      version = "2.20.8",
       char = '┊',
       show_trailing_blankline_indent = false,
     },
