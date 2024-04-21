e681c6721ff7d6030e629474f646c2947b0cdcc1
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2024-04-21 20:40:39 +0300

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -112,7 +112,14 @@ require('lazy').setup({
     },
   },
 
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "bluz71/vim-moonfly-colors", 
+    name = "moonfly", 
+    lazy = false, 
+    priority = 1000 
+    config = function()
+      vim.cmd.colorscheme 'moonfly'
+    end,
+  },
 
   { -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
